30b2282c5b07e14f36a9a31d759a9b947e00d393
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-03-20 15:41:07 -0700

    added main file

---
+++ muyang_and_nicole.py
@@ -0,0 +1,25 @@
+"""
+Nicole Hsu
+Muyang Li
+"""
+
+
+def replace_all(input_file, output_file, search, replace):
+    """
+
+    :param input_file:
+    :param output_file:
+    :param search:
+    :param replace:
+    :return:
+    """
+
+
+def main():
+    """
+    Drives the program.
+    """
+
+
+if __name__ == "__main__":
+    main()
