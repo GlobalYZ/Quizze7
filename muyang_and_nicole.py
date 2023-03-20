d8176f35799e5299903f5313256db82456401126
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-03-20 15:48:44 -0700

    implementing function body

--- muyang_and_nicole.py
+++ muyang_and_nicole.py
@@ -13,7 +13,9 @@ def replace_all(input_file, output_file, search, replace):
     :param replace:
     :return:
     """
-
+    with open(input_file) as file_object:
+        new_file = file_object.read()
+    print(new_file)
 
 def main():
     """
