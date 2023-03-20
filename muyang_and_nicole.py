978c437a5f710ab4ce00c2a605b96a9c08660412
GLobalYZ <globalyy2020@gmail.com>
2023-03-20 16:09:09 -0700

    added docstring

--- muyang_and_nicole.py
+++ muyang_and_nicole.py
@@ -6,13 +6,20 @@ Muyang Li
 
 def replace_all(input_file, output_file, search, replace):
     """
-    Replace
+    Replace all given search word with the replaced word in the given input file.
 
+    Generate the output content into the given output file.
 
-    :param input_file:
-    :param output_file:
-    :param search:
-    :param replace:
+    :param input_file: a string to indicate the input file name.
+    :param output_file: a string to indicate the output file name.
+    :param search: a string that needs to be replaced.
+    :param replace: a string that needs to replace the search parameter
+    :precondition: the input file that input_file parameter points to can not be empty
+    :precondition: the output file the input_file parameter points to must be empty before the function is called
+    :postcondition: the content with all given search word replaced correctly, and generated to the defined output file.
+    >>> replace_all("test.txt", "output.txt", "cat", "dog")
+    >>> 
+
     :return:
     """
 
